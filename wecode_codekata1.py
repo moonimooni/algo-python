1b6cf3b0b858ca87e25769da5b807f4951d75f28
moonimooni <munhw1408@gmail.com>
2021-03-29 11:58:18 +0900

    2021/03/29

--- wecode_codekata1.py
+++ wecode_codekata1.py
@@ -1,4 +1,8 @@
+import time
+start_time = time.time()
+
 def two_sum(nums, target):
+
     # 아래 코드를 작성해주세요.
 
     # 투포인터
@@ -11,7 +15,7 @@ def two_sum(nums, target):
     #     elif nums_[i_1] + nums_[i_2] > target:
     #         i_2 -= 1
     #     elif nums_[i_1] + nums_[i_2] == target:
-    #         return [nums.index(nums_[i_1]), nums.index(nums_[i_2])]
+    #         return [i_1, i_2]
     # return None
 
     for i in range(len(nums)):
@@ -20,4 +24,5 @@ def two_sum(nums, target):
                 return [i, j]
 
 
-print(two_sum([4,9,11,13], 17))
+print(two_sum([4,9,11,14,10,20,30,50,40,33,46,88,29,41,16], 13))
+print(f'걸린 시간: {(time.time() - start_time) * 10000}')
