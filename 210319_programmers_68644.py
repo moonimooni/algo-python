60f339e94c9dac619f59d28ea5988465b409ffec
moonimooni <munhw1408@gmail.com>
2021-06-17 12:29:08 +0900

    해쉬 문제 - 완주하지 못한 선수

--- 210319_programmers_68644.py
+++ 210319_programmers_68644.py
@@ -1,16 +1,5 @@
 from itertools import combinations
 
 def solution(numbers):
-    answer = []
-    
-    for i in range(len(numbers)):
-        for j in range(i+1, len(numbers)):
-            answer.append(numbers[i] + numbers[j])
-    
-    # pairs = combinations(numbers, 2)
-    # for pair in pairs:
-    #     answer.append(sum(pair))
-    
-    return sorted(list(set(answer)))
-            
-    return answer
+    pairs = combinations(numbers, 2)
+    return sorted(list(set([sum(pair) for pair in pairs])))
--- 210617_programmers_42576.py
+++ 210617_programmers_42576.py
@@ -1,22 +1,24 @@
 import hashlib
 
-participant = ['leo', 'kiki', 'eden']
-completion = ['eden', 'kiki']
+# participant = ['leo', 'kiki', 'eden']
+# completion = ['eden', 'kiki']
 # participant = ['marina', 'josipa', 'nikola', 'vinko', 'filipa']
 # completion = ['josipa', 'filipa', 'marina', 'nikola']
-# participant = ['mislav', 'stanko', 'mislav', 'ana']
-# completion = ['stanko', 'ana', 'mislav']
+participant = ['mislav', 'stanko', 'mislav', 'ana']
+completion = ['stanko', 'ana', 'mislav']
 
 def solution(participant, completion):
     hash_dict = dict()
 
     for name in participant:
         hash_adr = hash(name)
+
         if hash_adr in hash_dict.keys():
+            print(hash_dict[hash_adr])
             hash_dict[hash_adr].append(name)
         else:
             hash_dict[hash_adr] = [name]
-    
+
     for cpl in completion:
         hash_adr = hash(cpl)
         hash_dict[hash_adr].pop()
@@ -26,4 +28,17 @@ def solution(participant, completion):
     answer = list(hash_dict.values())[0][0]
     return answer
 
+# 다른 사람 풀이
+def solution(participant, completion):
+    answer = ""
+    temp = 0
+    dict_ = {}
+    for part in participant:
+        dict_[hash(part)] = part
+        temp += hash(part)
+    
+    for comp in completion:
+        temp -= hash(comp)
+    
+    return dict_[temp]
 print(solution(participant, completion))
