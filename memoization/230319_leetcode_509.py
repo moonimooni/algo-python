4f58fd1107f4efdb8a7d8808af1f088ffd608f78
문희원 <munhw1408@gmail.com>
2023-03-19 23:35:17 +0900

    dp 문제 : 피보나치

---
+++ memoization/230319_leetcode_509.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/fibonacci-number/
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        return self.fib(n-1) + self.fib(n-2)
+
+
+s = Solution()
+print(s.fib(10))
